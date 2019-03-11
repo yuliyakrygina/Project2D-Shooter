cdabf8d8a3ce4507c2fa10e798eeb50b65cc6a7e
Yuliya K <36555259+yuliyakrygina@users.noreply.github.com>
2019-03-11 14:39:20 -0400

    Adding shooting to scripts

---
+++ Project 2D Shooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public GameObject target;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		
+    }
+
+	void Spawn()
+	{
+		float RandomX = Random.Range(-7f, 7f);
+		float RandomY = Random.Range(-3f, 3f);
+
+		Vector3 RandomPosition = new Vector3(RandomX, RandomY, 0);
+
+		//add transform position
+		Instantiate(target, RandomPosition, Quaternion.identity);
+	}
+}
